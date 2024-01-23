888d6686ec246ab59e82063976eb5c5e2a29f8ad
Awesomelok622 <pzdn83@durham.ac.uk>
2024-01-23 14:31:09 +0000

    Update RL Training.py

--- rummikub-main/RL Training.py
+++ rummikub-main/RL Training.py
@@ -1,1 +1,13 @@
+env = RummikubEnv()
+agent = RummikubAgent()
+
+for episode in range(NUM_EPISODES):
+    state = env.reset()
+    done = False
+
+    while not done:
+        action = agent.sample_action(state)
+        next_state, reward = env.step(state, action)
+        agent.update(state, action, reward, next_state)
+        state = next_state
 
